1800bd6cd922ea284f40f3a7277b82df697f2560
Harsh-739 <harshpreet9721@gmail.com>
2025-09-26 23:47:30 +0530

    Added multiple data analysis projects

---
+++ Zomato Data Analysis/code.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('/Users/harshpreet/Desktop/data-analysis-projects/Zomato Data Analysis/Zomato-data-.csv')
+print(df.head())
